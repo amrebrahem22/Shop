32e8a6b9d8795d98a46cc26988b72bfd77d482e8
unknown <amrebrahem226@gmail.com>
2021-12-29 08:47:57 +0200

    create Category and Product models

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Product
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+	list_display = ['name', 'slug']
+	prepopulated_fields = { 'slug': ('name',) }
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+	list_display = ['title', 'slug', 'price', 'author', 'in_stock', 'created', 'updated']
+	prepopulated_fields = { 'slug': ('title',) }
+	list_filter = ['in_stock', 'is_active']
+	list_editable = ['price', 'in_stock']
+
