27408bc1427188baf4525ed43de42a55ed6ffa0a
unknown <amrebrahem226@gmail.com>
2022-01-06 07:32:37 +0200

    finish basket

--- basket/basket.py
+++ basket/basket.py
@@ -1,3 +1,6 @@
+from decimal import Decimal 
+from store.models import Product
+
 class Basket:
     """
     A base basket class, providing some default behaviors that can be inherited or overrided as necessary.
@@ -11,3 +14,68 @@ class Basket:
             basket = self.session['skey'] = {}
 
         self.basket = basket
+
+    def add(self, product, product_qty):
+        """
+        Adding and updating users basket session
+        """
+        product_id =str(product.id)
+
+        if product_id not in self.basket:
+            self.basket[product.id]['qty'] = product_qty
+        else:
+            self.basket[product_id] = {'price': str(product.price), 'qty': int(product_qty)}
+
+        self.save()
+
+    def __iter__(self):
+        """
+        Collect product ids in the session data to query the database and return products
+        """
+        product_ids = self.basket.keys()
+        products = Product.products.filter(id__in=product_ids)
+        basket = self.basket.copy()
+
+        for product in products:
+            basket[str(product.id)]['product'] = product
+
+        for item in basket.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['qty']
+            yield item
+
+    def __len__(self):
+        """
+        get the basket items count
+        """
+
+        return sum(item['qty'] for item in self.basket.values())
+
+    def get_total_price(self):
+        """
+        get the final price
+        """
+        return sum(Decimal(item['price']) * item['qty'] for item in self.basket.values())
+
+    def delete(self, product):
+        """
+        Delete item from session data
+        """
+        product_id = str(product)
+        
+        if product_id in self.basket:
+            del self.basket[product_id]
+            self.save()
+
+    def update(self, product, product_qty):
+        """
+        update item from session data
+        """
+        product_id = str(product)
+        
+        if product_id in self.basket:
+            self.basket[product_id]['qty'] = product_qty
+            self.save()
+
+    def save(self):
+        self.session.modified = True
--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
+
+from .basket import Basket
+from store.models import Product
 
 def basket_summary(request):
-    return render(request, 'store/basket/summary.html')
+    basket = Basket(request)
+    return render(request, 'store/basket/summary.html', {'basket': basket})
+
+
+def basket_add(request):
+    basket = Basket(request)
+
+    if request.POST.get('action') == 'post':
+        product_id = str(request.POST.get('productid'))
+        product_qty = str(request.POST.get('productqty'))
+        product = get_object_or_404(Product, id=product_id)
+        basket.add(product=product, product_qty=product_qty)
+
+        basketqty = basket.__len__()
+        response = JsonResponse({'qty': basketqty})
+        return response
+
+def basket_delete(request):
+    basket = Basket(request)
+
+    if request.POST.get('action') == 'delete':
+        product_id = str(request.POST.get('productid'))
+        basket.delete(product=product_id)
+
+        basketqty = basket.__len__()
+        baskettotal = basket.get_total_price()
+        response = JsonResponse({'qty': basketqty, 'subtotal': baskettotal})
+        return response
+
+def basket_update(request):
+    basket = Basket(request)
+
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('productid'))
+        product_qty = int(request.POST.get('productqty'))
+        basket.update(product=product_id, product_qty=product_qty)
+
+        basketqty = basket.__len__()
+        baskettotal = basket.get_total_price()
+        response = JsonResponse({'qty': basketqty, 'subtotal': baskettotal})
+        return response
+
