6671097cff819ac67d41e80d0ac22de5df9b296f
unknown <amrebrahem226@gmail.com>
2022-01-09 00:36:13 +0200

    edit & delete user

--- account/views.py
+++ account/views.py
@@ -3,11 +3,11 @@ from django.shortcuts import render, redirect
 from django.template.loader import render_to_string
 from django.contrib.sites.shortcuts import get_current_site
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.core.exceptions import ObjectDoesNotExist
-from .forms import RegistrationForm
+from .forms import RegistrationForm, UserEditForm
 from .models import UserBase
 from .token import account_activation_token
 
@@ -15,10 +15,30 @@ from .token import account_activation_token
 def dashboard(request):
     return render(request, 'account/user/dashboard.html')
 
+@login_required
+def edit_details(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+
+        if user_form.is_valid():
+            user_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+    
+    return render(request, 'account/user/edit_details.html', {'user_form': user_form})
+
+@login_required
+def delete_user(request):
+    user = UserBase.objects.get(user_name=request.user)
+    user.is_active = False
+    user.save()
+    logout(request)
+    return redirect('account:delete_confirmation')
+
 def account_register(request):
 
-    # if request.user.is_authenticated:
-    #     return redirect('account:dashboard')
+    if request.user.is_authenticated:
+        return redirect('account:dashboard')
 
     if request.method == 'POST':
         registerForm = RegistrationForm(request.POST)
